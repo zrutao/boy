70abbb786bb36d5811a4fc0a270cbb596311434b
zrutao <1585240709@qq.com>
2020-02-27 14:46:26 +0800

    新建的boy项目

---
+++ src/main/java/com/zrt/boy/BoyApplication.java
@@ -0,0 +1,13 @@
+package com.zrt.boy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoyApplication.class, args);
+    }
+
+}
